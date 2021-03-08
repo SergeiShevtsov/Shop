5e22844442dce3c1b4023eebe848f2e312217714
Sergey <Sea35Fox@gmail.com>
2021-03-08 17:40:41 +0300

    Add models books with comments and create some fields connections.

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from manager.models import Book, Comment
+
+
+class CommentAdmin(admin.StackedInline):
+    model = Comment
+    extra = 2
+
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [CommentAdmin]
+
+
+admin.site.register(Book, BookAdmin)
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
+from manager.models import Book
 
 
 def hello(request, digit=None):
@@ -7,5 +10,10 @@ def hello(request, digit=None):
         return HttpResponse(f"hello is {digit}")
     return HttpResponse("hello world")
 
+
+class MyPage(View):
+    def get(self, request):
+        context = {"books": Book.objects.all()}
+        return render(request, "index.html", context)
 
 # Create your views here.
